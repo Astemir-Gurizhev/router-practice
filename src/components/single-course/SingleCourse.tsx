bd1bc863f7525caee58c464c5d70d1b669ed44ca
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-01-29 10:02:34 +0300

    Added information on the specific course page

--- src/components/single-course/SingleCourse.tsx
+++ src/components/single-course/SingleCourse.tsx
@@ -1,5 +1,19 @@
+import { Link, useParams } from 'react-router'
+import courses from '../../data/courses'
+
 const SingleCourse = () => {
-	return <h1>Single Course Info</h1>
+	const params = useParams()
+	const course = courses.find(course => course.slug === params.slug)
+
+	return (
+		<>
+			<h1>{course?.title}</h1>
+			<h3>{course?.slug}</h3>
+			<Link to='..' relative='path' style={{
+				textDecoration: 'underline'
+			}}>All courses</Link>
+		</>
+	)
 }
 
 export default SingleCourse
