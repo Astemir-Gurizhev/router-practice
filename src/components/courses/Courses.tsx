2d06d570cc206365639fa3f05e165c5bbbbcdb8d
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-01-29 10:52:06 +0300

    Added functions for sorting courses

--- src/components/courses/Courses.tsx
+++ src/components/courses/Courses.tsx
@@ -1,11 +1,28 @@
-import { Link } from 'react-router'
+import queryString from 'query-string'
+import { useState } from 'react'
+import { Link, useLocation } from 'react-router'
 import courses from '../../data/courses'
 import styles from './Courses.module.css'
+
+const sortCourses = (courses, key) => {
+	const sortCourses = [...courses]
+	sortCourses.sort((a, b) => (a[key] > b[key] ? 1 : -1))
+	return sortCourses
+}
+
 const Courses = () => {
+	const location = useLocation()
+	const query = queryString.parse(location.search)
+	const [sortKey, setSortKey] = useState(query.sort)
+
+	const [sortedCourses, setSortedCourses] = useState(
+		sortCourses(courses, sortKey)
+	)
+
 	return (
 		<>
 			<h1>Courses</h1>
-			{courses.map(course => (
+			{sortedCourses.map(course => (
 				<div className={styles.course} key={course.id}>
 					<Link to={course.slug}>{course.title}</Link>
 				</div>
