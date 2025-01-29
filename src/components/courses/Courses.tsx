c0fad9612c22b124223f96261cee774414ea13b9
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-01-29 09:42:11 +0300

    Added links to individual courses

--- src/components/courses/Courses.tsx
+++ src/components/courses/Courses.tsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router'
+import courses from '../../data/courses'
+import styles from './Courses.module.css'
 const Courses = () => {
-	return <h1>Courses</h1>
+	return (
+		<>
+			<h1>Courses</h1>
+			{courses.map(course => (
+				<div className={styles.course} key={course.id}>
+					<Link to={course.slug}>{course.title}</Link>
+				</div>
+			))}
+		</>
+	)
 }
 
 export default Courses
