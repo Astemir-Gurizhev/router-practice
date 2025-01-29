dd483d8e78fdc6ec9d25a6275128f67b0f56ce1b
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-01-29 16:42:12 +0300

    Typing the Courses.tsx file

--- src/components/courses/Courses.tsx
+++ src/components/courses/Courses.tsx
@@ -1,48 +1,58 @@
-import queryString from 'query-string'
-import { useEffect, useState } from 'react'
-import { Link, useLocation, useNavigate } from 'react-router'
-import courses from '../../data/courses'
-import styles from './Courses.module.css'
+import queryString from 'query-string';
+import { useEffect, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router';
+import courses from '../../data/courses'; 
+import styles from './Courses.module.css';
 
-const SORT_KEYS = ['title', 'slug', 'id']
+type Course = {
+  id: number;
+  title: string;
+  slug: string;
+};
 
-const sortCourses = (courses, key) => {
-	const sortCourses = [...courses]
-	if (!key || !SORT_KEYS.includes(key)) {
-		return sortCourses
-	}
-	sortCourses.sort((a, b) => (a[key] > b[key] ? 1 : -1))
-	return sortCourses
-}
+type SortKeys = 'title' | 'slug' | 'id';
+
+const SORT_KEYS: SortKeys[] = ['title', 'slug', 'id'];
+
+const sortCourses = (courses: Course[], key?: SortKeys): Course[] => {
+  const sortCourses = [...courses];
+  if (!key || !SORT_KEYS.includes(key)) {
+    return sortCourses;
+  }
+  sortCourses.sort((a, b) => (a[key] > b[key] ? 1 : -1));
+  return sortCourses;
+};
 
 const Courses = () => {
-	const location = useLocation()
-	const query = queryString.parse(location.search)
-	const navigate = useNavigate()
-	const [sortKey, setSortKey] = useState(query.sort)
+  const location = useLocation();
+  const query = queryString.parse(location.search);
+  const navigate = useNavigate();
+  const [sortKey, setSortKey] = useState<SortKeys | undefined>(
+    query.sort as SortKeys | undefined
+  );
 
-	const [sortedCourses, setSortedCourses] = useState(
-		sortCourses(courses, sortKey)
-	)
+  const [sortedCourses, setSortedCourses] = useState<Course[]>(
+    sortCourses(courses, sortKey ?? undefined)
+  );
 
-	useEffect(() => {
-		if(!SORT_KEYS.includes(sortKey)) {
-			navigate('.')
-			setSortKey(null)
-			setSortedCourses([...courses])
-		}
-	}, [sortKey, navigate])
+  useEffect(() => {
+    if (!SORT_KEYS.includes(sortKey!)) {
+      navigate('.');
+      setSortKey(undefined);
+      setSortedCourses([...courses]);
+    }
+  }, [sortKey, navigate]);
 
-	return (
-		<>
-			<h1>{sortKey ? `Courses sorted by ${sortKey}` : 'Courses'}</h1>
-			{sortedCourses.map(course => (
-				<div className={styles.course} key={course.id}>
-					<Link to={course.slug}>{course.title}</Link>
-				</div>
-			))}
-		</>
-	)
-}
+  return (
+    <>
+      <h1>{sortKey ? `Courses sorted by ${sortKey}` : 'Courses'}</h1>
+      {sortedCourses.map((course) => (
+        <div className={styles.course} key={course.id}>
+          <Link to={course.slug}>{course.title}</Link>
+        </div>
+      ))}
+    </>
+  );
+};
 
-export default Courses
+export default Courses;
