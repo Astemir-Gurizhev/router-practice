60edbbc4027268a3f020510743ddc2b3a931a686
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-01-29 10:21:34 +0300

    Redirection using the useNavigate hook

--- src/components/single-course/SingleCourse.tsx
+++ src/components/single-course/SingleCourse.tsx
@@ -1,17 +1,30 @@
-import { Link, useParams } from 'react-router'
+import { useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router'
 import courses from '../../data/courses'
 
 const SingleCourse = () => {
 	const params = useParams()
 	const course = courses.find(course => course.slug === params.slug)
+	const navigate = useNavigate()
 
+	useEffect(() => {
+		if (!course) {
+			navigate('..', { relative: 'path' })
+		}
+	}, [course])
 	return (
 		<>
 			<h1>{course?.title}</h1>
 			<h3>{course?.slug}</h3>
-			<Link to='..' relative='path' style={{
-				textDecoration: 'underline'
-			}}>All courses</Link>
+			<Link
+				to='..'
+				relative='path'
+				style={{
+					textDecoration: 'underline',
+				}}
+			>
+				All courses
+			</Link>
 		</>
 	)
 }
